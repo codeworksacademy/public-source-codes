2cc9a58273a300df6427c7a066a4b1d959286036
Mick <beasticorn@gmail.com>
2024-09-11 08:25:47 -0600

    added unit 06, lecture 00 vue intro

---
+++ fullstack-online/unit_06/lectures/00_vueintro/src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { ref } from 'vue';
+import Tiger from './components/Tiger.vue';
+
+
+console.log("hello from the vue js");
+let counter = ref(0)
+
+function counterIncrease(){
+  counter.value++
+  console.log(counter.value)
+}
+
+let animals = ['🐅', '🦧', '🐘']
+
+</script>
+
+<template>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <section>
+      <h1>This is cool</h1>
+      <article>
+        Vue is so cool i think it's my new favorite
+        <button @click="counterIncrease()">Count</button>
+        <div>Count {{ counter }}</div>
+
+        <div v-if="counter > 20">You win!</div>
+        <div v-else>Keep going</div>
+
+        <div>
+          <span v-for="number in counter">{{ number }} 🐅</span>
+        </div>
+
+        <h1 v-for="animal in animals">
+          {{ animal }}
+        </h1>
+
+      </article>
+      <Tiger v-if="counter > 5"/>
+    </section>
+
+</template>
+
+<style scoped>
+
+article{
+  /* background-color: blue; */
+  color: rgb(222, 186, 255);
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+</style>
---
+++ fullstack-online/unit_06/lectures/00_vueintro/src/components/Tiger.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+console.log('rawr 🐅')
+
+</script>
+
+<template>
+  Hello from the tiger 🐅
+  <img src="https://em-content.zobj.net/source/microsoft-teams/363/tiger_1f405.png" alt="">
+</template>
